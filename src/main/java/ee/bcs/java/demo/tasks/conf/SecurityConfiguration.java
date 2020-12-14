b7bf1df4ae0f5657a95697b3e4d72b54effd5a34
Siim Rebane <siim@rebane.org>
2020-12-14 20:57:43 +0200

    Security

--- src/main/java/ee/bcs/java/demo/tasks/conf/SecurityConfiguration.java
+++ src/main/java/ee/bcs/java/demo/tasks/conf/SecurityConfiguration.java
@@ -18,11 +18,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/favicon.ico").permitAll()
-                .anyRequest().authenticated()
+                    .antMatchers("/", "/favicon.ico", "/register").permitAll()
+                    .anyRequest().authenticated()
                 .and()
-                .formLogin().permitAll()
+                    .formLogin().permitAll()
                 .and()
-                .logout().permitAll();
+                    .logout().permitAll();
     }
 }
