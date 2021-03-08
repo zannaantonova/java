fe56b7d4488588a70d65a386d6e16eb4ec189f7a
Siim Rebane <siim@rebane.org>
2021-03-08 19:54:46 +0200

    Lesson2b

---
+++ src/main/java/ee/bcs/java/demo/tasks/lesson1/Lesson2b.java
@@ -0,0 +1,25 @@
+package ee.bcs.java.demo.tasks.lesson1;
+
+public class Lesson2b {
+    public static void main(String[] args) {
+
+    }
+    // TODO trüki välja n arvu
+    public static void exercise1(){
+
+    }
+
+    // TODO tagasta massiiv milles oleks numbrid 1,2,3,4,5
+    public int[] sampleArray(){
+        return new int[0];
+    }
+
+    // TODO loo massiiv mis saab sisendiks n ja tagastab massiivi suurusega n
+    // Kus esimene element on 1 ja iga järnev arv on 1 võrra suurem
+    // näiteks:
+    // sisend: 5
+    // vastus: {1, 2, 3, 4, 5}
+    public static int[] generateArray(){
+        return new int[0];
+    }
+}
