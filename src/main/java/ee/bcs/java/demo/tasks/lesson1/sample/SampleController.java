a7e7a59be7d85b00c98d04e9bcab4ff517e53598
Siim Rebane <siim@rebane.org>
2021-03-24 19:29:31 +0200

    Dto ample

---
+++ src/main/java/ee/bcs/java/demo/tasks/lesson1/sample/SampleController.java
@@ -0,0 +1,16 @@
+package ee.bcs.java.demo.tasks.lesson1.sample;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SampleController {
+
+    @GetMapping("sample/test")
+    public EmployeeDto getSampleEmployee(){
+        EmployeeDto employee = new EmployeeDto();
+        employee.setFirstName("Siim");
+        employee.setLastName("Rebane");
+        return employee;
+    }
+}
