994e0a869cf8e647fafef9718c47b73452c3f508
Siim Rebane <siim@rebane.org>
2021-04-12 19:59:59 +0300

    repo näidis

--- src/main/java/ee/bcs/java/demo/tasks/lesson1/sample/bank/SampleBankRepository.java
+++ src/main/java/ee/bcs/java/demo/tasks/lesson1/sample/bank/SampleBankRepository.java
@@ -2,21 +2,21 @@ package ee.bcs.java.demo.tasks.lesson1.sample.bank;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Service
-public class SampleBankService {
+@Repository
+public class SampleBankRepository {
 
     @Autowired
     private NamedParameterJdbcTemplate jdbcTemplate;
 
-    @Autowired
-    private SampleBankRepository bankRepository;
-
     public void createAccount(String accountNumber){
-        bankRepository.createAccount(accountNumber);
+        String sql = "INSERT INTO account (account_number, balance) VALUES (:accountNumber, 0)";
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("accountNumber", accountNumber);
+        jdbcTemplate.update(sql, paramMap);
     }
 }
