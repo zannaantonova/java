e4b930648d7ebe6cc837db7cd55806cd17579ecb
Siim Rebane <siim@rebane.org>
2021-04-12 18:50:20 +0300

    old bank controller

--- src/main/java/ee/bcs/java/demo/tasks/lesson1/sample/bank/SampleBankControllerWithoutService.java
+++ src/main/java/ee/bcs/java/demo/tasks/lesson1/sample/bank/SampleBankControllerWithoutService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
 
-@RequestMapping("sample")
+@RequestMapping("sample_old")
 @RestController
 public class SampleBankControllerWithoutService {
 
