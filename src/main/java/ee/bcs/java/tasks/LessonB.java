354e42d8ba0b023784d1b8872cd0a031a359be39
Siim Rebane <siim@rebane.org>
2021-09-27 20:25:57 +0300

    more lessons

---
+++ src/main/java/ee/bcs/java/tasks/LessonB.java
@@ -0,0 +1,28 @@
+package ee.bcs.java.tasks;
+
+public class LessonB {
+
+    public static void main(String[] args) {
+        // TODO kasuta scannerit nii, et mainist oleks võimalik kõiki meetodeid välja kutsuda
+        // Täpselt nii nagu Lesson 1 main meetod
+
+    }
+
+    public String checkOrder(int a, int b, int c){
+        // Print "increasing" if c > b > a
+        // Print "decreasing" if c < b < a
+        // Print "neither" if none of them is true
+        return "";
+    }
+
+    public boolean checkEqual(int a, int b, int c){
+        // return true if all 3 parameters are the same
+        return false;
+    }
+
+    public boolean isSamll(int a){
+        // return true if the absolute number of a is smaller than 1000
+        return false;
+    }
+
+}
