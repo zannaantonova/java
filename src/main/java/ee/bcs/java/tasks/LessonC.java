2500c965f7eab163bff3aa41957d67df7abf555c
Siim Rebane <siim@rebane.org>
2021-09-29 17:56:52 +0300

    more lessons

---
+++ src/main/java/ee/bcs/java/tasks/LessonC.java
@@ -0,0 +1,22 @@
+package ee.bcs.java.tasks;
+
+public class LessonC {
+    public static void main(String[] args) {
+
+    }
+
+    // TODO korruta omavahel x ja z ning liida sellele y ja z korrutis
+    public static int testMath(int x, int y, int z){
+        return 0;
+    }
+
+    // TODO kui mitu paaris arvu on sisendites
+    public static int testStuff(int a, int b, int c, int d, int e, int f, int g){
+        return 0;
+    }
+
+    public static boolean userValidation(boolean emailValid, boolean telephoneValid, boolean addressValid){
+        // tagasta true, kui muutuja addressValid on tõene ja vähemalt üks (emailValid või telephoneValid) on tõene
+        return false;
+    }
+}
---
+++ src/main/java/ee/bcs/java/tasks/LessonD.java
@@ -0,0 +1,54 @@
+package ee.bcs.java.tasks;
+
+public class LessonD {
+    public static void main(String[] args) {
+
+    }
+
+    // Kõiki alljärgnevaid ülesandeid lahenda kasutades tsükleid
+    // TODO trüki numbrid 1 -> 10
+    public static void yl1(){
+
+    }
+
+    // TODO trüki numbrid 10 -> 1
+    public static void yl2(){
+
+    }
+
+    // TODO trüki paaris numberid 2->20
+    public static void yl3(){
+
+    }
+
+    // TODO trüki numbrid 1 -> n
+    public static void yl4(int n){
+
+    }
+
+    // TODO trüki numbrid n -> 1
+    public static void yl5(int n){
+
+    }
+
+    // TODO trüki paaris numberid 2->n
+    public static void yl6(int n){
+
+    }
+
+    // TODO kasutades Scannerit loe sisse üks number, korruta see 2-ga ja trüki välja
+    // TODO2 kasutades while loopi, tee seda nii kaua kuni kasutaja sisestab 0-i
+    public static void yl7(){
+
+    }
+
+    // TODO täienda yl1 main meetodit
+    // TODO tee nii, et kasutaja saaks lõputult valida uusi valikuid
+    // TODO lisa uus käsklus (exit) valikusse
+    // TODO kui kasutaja valib exit käsu siis lõpeta programmi töö
+    public static void yl8(){
+
+    }
+
+    // TODO jätka Lesson3Hard
+}
