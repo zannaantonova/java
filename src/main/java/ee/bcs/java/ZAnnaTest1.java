9b8a37bb527bdc09e49bb108dda2d772298e4685
ZA <zannaantonova@gmail.com>
2021-11-17 16:47:43 +0200

    näidis commit

---
+++ src/main/java/ee/bcs/java/ZAnnaTest1.java
@@ -0,0 +1,13 @@
+package ee.bcs.java;
+
+public class ZAnnaTest1 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 6;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
